d49aa80ae50cf3e0e59398cee78e040b88a678e7
emirrtopaloglu <emirrttopaloglu@gmail.com>
2024-02-18 00:50:16 +0300

    added redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import "@/src/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import StoreProvider from "../context/StoreProvider";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
     </QueryClientProvider>
   );
 }

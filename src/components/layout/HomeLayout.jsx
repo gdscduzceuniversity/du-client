e6e828ab3c68a1497e4771ab32a99eb62333de21
emirrtopaloglu <emirrttopaloglu@gmail.com>
2024-02-24 17:14:10 +0300

    Add FooterComponent to HomeLayout

--- src/components/layout/HomeLayout.jsx
+++ src/components/layout/HomeLayout.jsx
@@ -1,11 +1,12 @@
+import FooterComponent from "./Footer";
 import HeaderComponent from "./Header";
 
 export default function HomeLayout({ children }) {
   return (
-    <div>
+    <>
       <HeaderComponent />
       {children}
-      <footer>footer content</footer>
-    </div>
+      <FooterComponent />
+    </>
   );
 }

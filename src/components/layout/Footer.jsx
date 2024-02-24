26d07a6e41a7b637facad581750d6d8229cc6389
emirrtopaloglu <emirrttopaloglu@gmail.com>
2024-02-24 17:14:00 +0300

    Add Footer component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function FooterComponent() {
+  return (
+    <footer className="inset-x-0 top-0 z-50 bg-white border-t bg-gray-900">
+      <div className="container flex items-center text-white text-sm text-center text-gray-400 justify-center h-14 px-4 md:px-6">
+        DÜSosyal &copy; Tüm hakları saklıdır.
+      </div>
+    </footer>
+  );
+}

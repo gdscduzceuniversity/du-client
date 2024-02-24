a9472c57b863aa1cda3bc5bc56292cc7ae10e96a
emirrtopaloglu <emirrttopaloglu@gmail.com>
2024-02-24 17:39:51 +0300

    Remove redundant CSS class from Footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export default function FooterComponent() {
   return (
-    <footer className="inset-x-0 top-0 z-50 bg-white border-t bg-gray-900">
+    <footer className="inset-x-0 top-0 z-50 border-t bg-gray-900">
       <div className="container flex items-center text-white text-sm text-center text-gray-400 justify-center h-14 px-4 md:px-6">
         DÜSosyal &copy; Tüm hakları saklıdır.
       </div>
